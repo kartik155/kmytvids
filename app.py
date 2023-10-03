abd9c5b5e34635038475c3d849b18a18b1ff9439
Kartik Mehboobani <kartik.mehboobani@beghouconsulting.com>
2023-10-03 16:04:02 +0530

    new_vids

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from  streamlit_player import st_player
 
 import pandas as pd
 
-df = pd.read_excel("processed_links.xlsx")
-df.drop_duplicates(inplace = True)
+df_in = pd.read_excel("processed_links.xlsx")
+df_in.drop_duplicates(inplace = True)
 
 if __name__ == "__main__":
     import os
@@ -15,7 +15,20 @@ if __name__ == "__main__":
     st.set_page_config(layout="wide", page_title = "Videos worth watching!")
 
     top_col = st.columns(3)
-    top_col[1].header(f'{df.shape[0]} Videos worth watching!')
+
+    cats = df_in["Category"].unique().tolist()
+
+    with top_col[0]:
+        tags = st.multiselect('Which all categories you want to see?', cats, [])
+
+    if tags == []:
+        df = df_in.copy()
+    else:
+        filter_condition = df_in['Category'].isin(tags)
+        df = df_in[filter_condition].copy().reset_index(drop = True)
+        # print(df.head())
+
+    top_col[2].header(f'{df.shape[0]} Videos worth watching!')
     st.divider()
 
     for i in range(df.shape[0]):
@@ -27,8 +40,10 @@ if __name__ == "__main__":
             sub_cols = st.columns(2)
             with sub_cols[0]:
                 st.subheader("Publish Date : ")
+                st.subheader("Category : ")
             with sub_cols[1]:
                 st.subheader(df.loc[i,"Publish time"].split("T")[0])
+                st.subheader(df.loc[i,"Category"])
             st_player(df.loc[i,"Links"], key = f"player_{i}")
             
         with cols[1]:            
